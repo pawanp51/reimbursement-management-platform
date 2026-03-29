eb49f47b52882f8553398f4bd4926b16bbaf5d74
pawanp51 <pawanrp51@gmail.com>
2026-03-29 10:56:30 +0530

    Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-3xl hover:bg-sky-700 font-bold underline'>
+      Hello
+    </div>
+  )
+}
+
+export default App
