87180c6b277c83c30b65dfa81bc37a1b274d08f8
Mastermind730 <sourav.97032@gmail.com>
2026-03-29 16:29:01 +0530

    changes made in app.jsx for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
+import DashboardPage from './pages/manager/DashboardPage';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import './App.css';
@@ -11,8 +12,8 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/manager" element={<AdminDashboard />} />
-        <Route path="/admin" element={<Navigate to="/manager" replace />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/manager" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
